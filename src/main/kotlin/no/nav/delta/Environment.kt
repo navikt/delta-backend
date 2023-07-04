f6f889e03db08cf91f42798f8fa5edf0a9c80757
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-04 09:45:50 +0200

    change credentials to delta delta delta

--- src/main/kotlin/no/nav/delta/Environment.kt
+++ src/main/kotlin/no/nav/delta/Environment.kt
@@ -1,11 +1,11 @@
 package no.nav.delta
 
 data class Environment (
-    val dbUsername: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_USERNAME", "delta_backend"),
-    val dbPassword: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_PASSWORD", "ioDahceix5phiYoo6bee"),
+    val dbUsername: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_USERNAME", "delta"),
+    val dbPassword: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_PASSWORD", "delta"),
     val dbHost: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_HOST", "localhost"),
     val dbPort: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_PORT", "5432"),
-    val dbName: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_DATABASE", "delta_backend"),
+    val dbName: String = getEnvVar("NAIS_DATABASE_DELTA_BACKEND_DELTA_DATABASE", "delta"),
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
 ){
     fun jdbcUrl() = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
