2a14a420527460fe6a27f38b96810de01718256e
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-10 15:14:04 +0200

    remove unused import

--- src/main/kotlin/no/nav/delta/event/Database.kt
+++ src/main/kotlin/no/nav/delta/event/Database.kt
@@ -5,7 +5,6 @@ import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Timestamp
 import java.util.UUID
-import javax.xml.crypto.Data
 
 fun DatabaseInterface.getFutureEvents(): List<Event> {
     val events = ArrayList<Event>()
