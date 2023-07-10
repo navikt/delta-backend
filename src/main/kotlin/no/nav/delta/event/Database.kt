b61a940cb1f6187bc0852c6c210e18442410cb55
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-10 12:21:59 +0200

    refactor to use explicit variable names

--- src/main/kotlin/no/nav/delta/event/Database.kt
+++ src/main/kotlin/no/nav/delta/event/Database.kt
@@ -3,20 +3,17 @@ package no.nav.delta.event
 import no.nav.delta.plugins.DatabaseInterface
 import java.sql.ResultSet
 import java.sql.Timestamp
-import java.util.UUID
+import java.util.*
 
 fun DatabaseInterface.getEvents(): List<Event> {
     val events = ArrayList<Event>()
-    connection.use {
-        it.prepareStatement("SELECT * FROM event;")
-            .use {
-                it.executeQuery().use {
-                    while (it.next()) {
-                        val event = resultSetToEvent(it)
-                        events.add(event)
-                    }
-                }
-            }
+    connection.use { connection ->
+        val preparedStatement = connection.prepareStatement("SELECT * FROM event;")
+        val result = preparedStatement.executeQuery()
+        while (result.next()) {
+            val event = resultSetToEvent(result)
+            events.add(event)
+        }
     }
     return events
 }
@@ -28,30 +25,29 @@ fun DatabaseInterface.addEvent(
     startTime: Timestamp,
     endTime: Timestamp
 ) {
-    connection.use {
-        it.prepareStatement("INSERT INTO event(owner, title, description, start_time, end_time) VALUES (?, ?, ?, ?, ?);")
-            .use {
-                it.setString(1, ownerEmail)
-                it.setString(2, title)
-                it.setString(3, description)
-                it.setTimestamp(4, startTime)
-                it.setTimestamp(5, endTime)
-                it.executeUpdate()
-            }
-        it.commit()
+    connection.use { connection ->
+        val preparedStatement =
+            connection.prepareStatement(
+                "INSERT INTO event(owner, title, description, start_time, end_time) VALUES (?, ?, ?, ?, ?);"
+            )
+        preparedStatement.setString(1, ownerEmail)
+        preparedStatement.setString(2, title)
+        preparedStatement.setString(3, description)
+        preparedStatement.setTimestamp(4, startTime)
+        preparedStatement.setTimestamp(5, endTime)
+        preparedStatement.executeUpdate()
+        connection.commit()
     }
 }
 
 fun DatabaseInterface.getEvent(id: String): Event? {
-    connection.use {
-        it.prepareStatement("SELECT * FROM event WHERE id=uuid(?)")
-            .use {
-                it.setString(1, id)
-                val result = it.executeQuery()
+    connection.use { connection ->
+        val preparedStatement = connection.prepareStatement("SELECT * FROM event WHERE id=uuid(?)")
+        preparedStatement.setString(1, id)
+        val result = preparedStatement.executeQuery()
 
-                if (!result.next()) return null
-                return resultSetToEvent(result)
-            }
+        if (!result.next()) return null
+        return resultSetToEvent(result)
     }
 }
 
