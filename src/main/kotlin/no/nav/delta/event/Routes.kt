cf400e9cc37a8fa452b1e755009ae4bf435f98ce
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-10 10:15:22 +0200

    remove unused console.log

--- src/main/kotlin/no/nav/delta/event/Routes.kt
+++ src/main/kotlin/no/nav/delta/event/Routes.kt
@@ -44,8 +44,6 @@ fun Route.eventApi(database: DatabaseInterface) {
 
                 val principal = call.principal<JWTPrincipal>()!!
                 val ownerEmail = principal["preferred_username"]!!.lowercase()
-                println(createEvent.startTime)
-                println(createEvent.startTime.toInstant())
 
                 database.addEvent(
                     ownerEmail,
