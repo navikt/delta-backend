aee2f5c4809094546f6fd4bc75c6b19535c14891
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-26 16:03:32 +0200

    add update event notification
    Co-authored-by: Eline Jørgensen <eline.jorgensen@nav.no>

--- src/main/kotlin/no/nav/delta/email/Email.kt
+++ src/main/kotlin/no/nav/delta/email/Email.kt
@@ -4,6 +4,10 @@ import no.nav.delta.event.Event
 import no.nav.delta.event.Participant
 import no.nav.delta.plugins.EmailClient
 
+const val footer = """Vennlig hilsen,
+Team ΔDelta
+"""
+
 fun EmailClient.sendJoinConfirmation(
     event: Event,
     participant: Participant,
@@ -21,9 +25,31 @@ Sted: ${event.location}
 
 Detaljert oppdatert informasjon finnes på på https://delta.nav.no/event/${event.id}. Kontakt arrangøren${if (hosts.size == 1) "" else "e" } om du har spørsmål.
 
-Hilsen,
-Team ΔDelta
+$footer
 """
 
     sendEmail(subject = subject, body = email, toRecipients = listOf(participant.email))
+}
+
+fun EmailClient.sendUpdateNotification(
+    event: Event,
+    participants: List<Participant>,
+    hosts: List<Participant>
+) {
+    val subject = "Oppdatering på ${event.title}"
+    val email =
+        """Hei!
+Arrangementet ${event.title} har blitt oppdatert.
+
+Her er den oppdaterte informasjonen:
+Arrangør${if (hosts.size == 1) "" else "er" }: ${hosts.joinToString(", ") { it.email }}
+Tidspunkt: ${event.startTime} - ${event.endTime}
+Sted: ${event.location}
+
+Detaljert oppdatert informasjon finnes på på https://delta.nav.no/event/${event.id}. Kontakt arrangøren${if (hosts.size == 1) "" else "e" } om du har spørsmål.
+
+$footer
+"""
+
+    sendEmail(subject = subject, body = email, bccRecipients = participants.map { it.email })
 }
