35a8af26ab8ce662061c97e8d025ab34bdcc6b00
albrektsson <emil.albrektsson@nav.no>
2026-03-25 07:57:42 +0100

    testfix

--- src/test/kotlin/no/nav/delta/event/DatabasesTest.kt
+++ src/test/kotlin/no/nav/delta/event/DatabasesTest.kt
@@ -192,7 +192,7 @@ class DatabasesTest {
         val start = LocalDateTime.now().plusDays(7).withHour(10).withMinute(0).withSecond(0).withNano(0)
         val createEvent =
             futureEventTest("recurring-monthly", start).copy(
-                recurrence = RecurrenceRequest(RecurrenceFrequency.MONTHLY, start.toLocalDate().plusMonths(2)),
+                recurrence = RecurrenceRequest(RecurrenceFrequency.MONTHLY, start.toLocalDate().plusMonths(2).plusDays(6)),
             )
 
         val createdSeries =
